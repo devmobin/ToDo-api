39a9410b2c7d9311521cf56499bf0adfd432133b
devmobin <mobinheydari114@gmail.com>
2019-09-18 22:07:29 +0430

    user routes added

--- src/routers/user.js
+++ src/routers/user.js
@@ -2,4 +2,18 @@ const express = require('express')
 
 const router = express.Router()
 
+router.post('/user/signup', async (req, res) => {})
+
+router.post('/user/login', async (req, res) => {})
+
+router.get('/user/me', async (req, res) => {})
+
+router.patch('/user/me', async (req, res) => {})
+
+router.delete('/user/me', async (req, res) => {})
+
+router.get('/user/logout', async (req, res) => {})
+
+router.get('/user/logoutAll', async (req, res) => {})
+
 module.exports = router
