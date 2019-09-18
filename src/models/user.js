ac7e46d478c9ecc3a065fa7ead1f727c46713558
devmobin <mobinheydari114@gmail.com>
2019-09-18 22:53:15 +0430

    user model added

---
+++ src/models/user.js
@@ -0,0 +1,50 @@
+const mongoose = require('mongoose')
+
+const tokenSchema = new mongoose.Schema(
+  {
+    token: {
+      type: String,
+      required: true
+    }
+  },
+  {
+    _id: false
+  }
+)
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    trim: true
+  },
+  username: {
+    type: String,
+    unique: true,
+    required: true,
+    trim: true
+  },
+  email: {
+    type: String,
+    unique: true,
+    trim: true,
+    lowercase: true,
+    required: true,
+    validate(value) {}
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+    minlength: 7,
+    validate(value) {}
+  },
+  tokens: [tokenSchema]
+})
+
+userSchema.virtual('tasks', {
+  ref: 'Task',
+  localField: '_id',
+  foreignField: 'owner'
+})
+
+module.exports = mongoose.model('User', userSchema)
