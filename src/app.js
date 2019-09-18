c5cc98a1dcd0d2a4a079dcc3d2a0636772a43125
devmobin <mobinheydari114@gmail.com>
2019-09-18 19:36:52 +0430

    setup mongoose

--- src/app.js
+++ src/app.js
@@ -1,13 +1,22 @@
 const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
+const config = require('../config/config')
 const userRouter = require('./routers/user')
 const taskRouter = require('./routers/task')
 
 const app = express()
 
 app.use(express.json())
+app.use(cors())
 
 app.use(userRouter)
 app.use(taskRouter)
+
+mongoose.connect(config.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 module.exports = app
