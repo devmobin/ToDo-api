f7924076c7e1d5adfbe576042e19d8d7e5db5297
devmobin <mobinheydari114@gmail.com>
2019-09-18 22:12:06 +0430

    task routes added

--- src/routers/task.js
+++ src/routers/task.js
@@ -2,4 +2,14 @@ const express = require('express')
 
 const router = express.Router()
 
+router.post('/task/new', async (req, res) => {})
+
+router.get('/task/user', async (req, res) => {})
+
+router.get('/task/:id', async (req, res) => {})
+
+router.patch('/task/:id', async (req, res) => {})
+
+router.delete('/task/:id', async (req, res) => {})
+
 module.exports = router
