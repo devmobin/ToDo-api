b93f6c0d00162f65e9e6e16f565dd8dd519972db
devmobin <mobinheydari114@gmail.com>
2019-09-19 18:03:01 +0430

    prevent duplicated username and email

--- src/middlewares/validator/user.js
+++ src/middlewares/validator/user.js
@@ -1,6 +1,8 @@
 const validator = require('validator')
 
-const signupValidation = (req, res, next) => {
+const User = require('../../models/user')
+
+const signupValidation = async (req, res, next) => {
   if (!req.body.username || !req.body.email || !req.body.password) {
     return res
       .status(400)
@@ -9,6 +11,16 @@ const signupValidation = (req, res, next) => {
 
   if (!validator.isEmail(req.body.email)) {
     return res.status(400).send({ error: 'please enter valid email' })
+  }
+
+  const checkUsername = await User.findOne({ username: req.body.username })
+  if (checkUsername) {
+    return res.status(400).send({ error: 'username is already taken' })
+  }
+
+  const checkUserEmail = await User.findOne({ email: req.body.email })
+  if (checkUserEmail) {
+    return res.status(400).send({ error: 'email is used before' })
   }
 
   if (
