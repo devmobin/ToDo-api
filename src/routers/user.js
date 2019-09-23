a971295e82002876cd6187144734f50487b1426d
devmobin <mobinheydari114@gmail.com>
2019-09-23 18:28:44 +0330

    edit user profile

--- src/routers/user.js
+++ src/routers/user.js
@@ -34,7 +34,30 @@ router.post('/login', validator.loginValidation, async ({ body }, res) => {
 
 router.get('/me', auth, async ({ user }, res) => res.send(user))
 
-router.patch('/me', auth, async ({ body, user }, res) => {})
+router.patch(
+  '/me',
+  auth,
+  validator.editUserProfile,
+  async ({ body, user }, res) => {
+    const updates = Object.keys(body)
+    const allowedUpdates = ['name', 'email', 'password', 'username']
+    const isValidOperation = updates.every(update =>
+      allowedUpdates.includes(update)
+    )
+
+    if (!isValidOperation) {
+      return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+      updates.forEach(update => (user[update] = body[update]))
+      await user.save()
+      res.send(user)
+    } catch (e) {
+      res.status(500).send()
+    }
+  }
+)
 
 router.delete('/me', auth, async ({ body }, res) => {})
 
